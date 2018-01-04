47e244df40c9f377c93e04dc8aeed14169eb3c10
lucasdb <lukasdeb27@gmail.com>
2018-01-04 01:24:37 -0300

    Changed test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './components/items/items.component';
+import { ShoppingcartComponent } from './components/shoppingcart/shoppingcart.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        ItemsComponent,
+        ShoppingcartComponent
       ],
     }).compileComponents();
   }));
@@ -13,15 +17,9 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Angular Tutorial'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(app.title).toEqual('Antular Tutorial');
   }));
 });
